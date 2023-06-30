2a4b43b90b671795d429ace81bb31fba86f4a12e
ARMR <72846289+xARMR@users.noreply.github.com>
2023-06-30 14:27:41 +0300

    Update Find-subdomain.py

--- Find-subdomain.py
+++ Find-subdomain.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import pyfiglet
-import os
+from urllib.parse import urlparse, urljoin
 
 a = "\033[0m"
 b = "\033[31m"
@@ -16,10 +15,8 @@ j = "\033[92m"
 k = "\033[90m"
 l = "\033[94m"
 
-os.system('clear')
 print('+'*67)
-logo = pyfiglet.figlet_format("Find-Subdomain")
-print(logo)
+print("Find-Subdomain")
 print('+'*67)
 
 url = input("\n[?] Enter website: "+b)
@@ -27,7 +24,16 @@ if not url.startswith("https://"):
     url = "https://" + url
 
 print(j+'-'*67)
-response = requests.get(url)
+
+# Set the headers to simulate a browser request
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
+}
+
+# Send a GET request to the website and manually solve any security challenges if prompted
+response = requests.get(url, headers=headers)
+
+# Retrieve the HTML content after solving the security challenges
 html_content = response.text
 
 soup = BeautifulSoup(html_content, "html.parser")
@@ -35,6 +41,8 @@ soup = BeautifulSoup(html_content, "html.parser")
 links = soup.find_all("a")
 for link in links:
     href = link.get("href")
-    print(c+href)
+    absolute_url = urljoin(url, href)
+    print(c + absolute_url)
+
 print(i+'-'*67)
-print(d+"By ARMR")
+print("By ARMR")
